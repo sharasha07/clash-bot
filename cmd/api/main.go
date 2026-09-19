e780cf4fa68141f719877b946daa5edd30555d94
sharasha07 <sabasharashenidze07@gmail.com>
2026-09-19 16:16:10 +0400

    add parsing environment variables into config struct

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/caarlos0/env"
+)
+
+type Config struct {
+	Port int `env:"PORT,required"`
+}
+
+func main() {
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(cfg.Port)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sharasha07/clash-bot
 
 go 1.27.0
+
+require github.com/caarlos0/env v3.5.0+incompatible // indirect
