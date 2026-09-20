4c505faf5ceeb83bfba703b5f41d96aec7fbd37d
sharasha07 <sabasharashenidze07@gmail.com>
2026-09-20 17:27:07 +0400

    feat: add health handler test

---
+++ cmd/api/health_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/sharasha07/clash-bot/internal/assert"
+)
+
+func TestHealth(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/health", nil)
+	rr := httptest.NewRecorder()
+
+	app := newTestApplication()
+
+	app.health(rr, req)
+	resp := rr.Result()
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
+
+	var result struct {
+		Status string `json:"status"`
+	}
+
+	err := json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "available", result.Status)
+}
